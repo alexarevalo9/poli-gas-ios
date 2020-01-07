feec38dea0fb36952058786b67e784dcbcafe6ad
Alex Arevalo <arevaloalex9@gmail.com>
2020-01-07 07:23:37 -0500

    Add tab bar controller and navigation controller

--- Poligas/ViewController.swift
+++ Poligas/ViewController.swift
@@ -7,14 +7,47 @@
 //
 
 import UIKit
+import FirebaseAuth
+
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func loginButton(_ sender: Any) {
+        
+        guard let email = emailTextField.text,
+            let password = passwordTextField.text else {
+            return
+        }
+        Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
+            print("Result: \(result)")
+            print("Error: \(error)")
+            
+            if error != nil {
+                self.presentAlertWhit(title: "Error", message: error?.localizedDescription ?? "Ups! An Error Ocurred")
+            } else {
+                self.performSegue(withIdentifier: "loginSuccessSegue", sender: self)
+            }
+        }
+    }
+    
+    private func presentAlertWhit(title : String, message : String){
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAlertAction = UIAlertAction(title: "OK", style: .default) { _ in
+            self.emailTextField.text = ""
+            self.passwordTextField.text = ""
+            self.emailTextField.becomeFirstResponder()
+        }
+        
+        alertController.addAction(okAlertAction)
+        present(alertController, animated: true, completion: nil)
+    }
+    
 }
 
